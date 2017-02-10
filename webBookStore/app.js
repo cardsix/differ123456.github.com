9eae279d94abb4e53d4037f6f8cbc30a1827582c
differ_wy <differ_wy>
2017-02-10 19:09:46 +0800

    添加ejs模板之后测试路由访问

--- webBookStore/app.js
+++ webBookStore/app.js
@@ -1,15 +1,22 @@
 const koa = require('koa');
-var router = require('koa-route');
-var service = require('./service/webAppService.js');
-
 var app = koa();
 
-app.use(router.get('/test',function *(){
+var koaRoute = require('koa-route');
+
+var coViews = require('co-views');
+var rander = coViews('./view',{
+	map : { html : 'ejs'}
+});
+
+var service = require('./service/webAppService.js');
+
+
+app.use(koaRoute.get('/test',function *(){
 	this.body = service.test();
 }));
 
-app.use(function *(){
-  this.body = 'Hello Koa';
-});
+app.use(koaRoute.get('/',function *(){
+	this.body = yield rander('index',{title:'this is index'});
+}));
 
 app.listen(3001);
--- webBookStore/service/webAppService.js
+++ webBookStore/service/webAppService.js
@@ -3,3 +3,7 @@ const fs = require('fs');
 exports.test = function(){
 	return fs.readFileSync('./mock/test.json','utf8');
 };
+
+exports.returnIndex = function(){
+	return fs.readFileSync('./mock/home.json','utf8');
+};
