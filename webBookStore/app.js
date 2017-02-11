1c5d0527c496ce131bb75876868bf707d0049d0b
differ_wy <differ_wy>
2017-02-11 13:42:30 +0800

    添加静态资源的中间件，ejs模块化书城主页

--- webBookStore/app.js
+++ webBookStore/app.js
@@ -1,22 +1,38 @@
 const koa = require('koa');
 var app = koa();
 
-var koaRoute = require('koa-route');
+var koa_route = require('koa-route');
 
-var coViews = require('co-views');
-var rander = coViews('./view',{
+var views = require('co-views');
+var rander = views('./view',{
 	map : { html : 'ejs'}
 });
 
+var koa_static = require('koa-static-server');
+app.use(koa_static({
+	rootDir:'./static',
+	rootPath:'/static/',
+	maxage:0
+}));
+
 var service = require('./service/webAppService.js');
 
-
-app.use(koaRoute.get('/test',function *(){
+app.use(koa_route.get('/test',function *(){
 	this.body = service.test();
 }));
 
-app.use(koaRoute.get('/',function *(){
-	this.body = yield rander('index',{title:'this is index'});
+//view router settings
+app.use(koa_route.get('/',function *(){
+	this.body = yield rander('index',{
+		title:'书城主页',
+		testData:service.returnIndex()
+	});
+}));
+app.use(koa_route.get('/book',function *(){
+	this.body = yield rander('book',{title:'书籍详情页'});
+}));
+app.use(koa_route.get('/search',function *(){
+	this.body = yield rander('search',{title:'搜索页'});
 }));
 
 app.listen(3001);
